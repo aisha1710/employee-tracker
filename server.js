58a08afc98531c869a03358f37cee742694e34bc
aisha <faisha879@gmail.com>
2022-11-27 20:36:24 +0000

    initial server.js commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const db = require("./db");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "employee_db",
+  },
+  console.log(`Connected to the employee_db database.`)
+);
